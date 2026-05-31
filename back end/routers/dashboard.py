448d03eb80e0c83475c153721927d8343f630473
luizalves10-00 <luiz.alves10@aluno.senai.br>
2026-05-31 13:55:53 -0300

    FiX: Atualização da comunicação do banco com o sistema web

--- back end/routers/dashboard.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from services.dashboard import DashboardService
-from schemas import KPIs
-
-router = APIRouter(prefix="/dashboard", tags=["dashboard"])
-
-@router.get("/kpis", response_model=KPIs)
-def obter_kpis():
-    return DashboardService().obter_kpis()
